39288b93995346cc2b8bdcc57705dadc7c815193
Miray Karagöz <mmiraykaragoz@hotmail.com>
2024-05-15 17:59:32 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using MagazaSitesi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MagazaSitesi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
